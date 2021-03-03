f4337946410b68c332eb45a54b97ae2d001c5738
Admin <alexandre.kimtsaris@bstorm.be>
2021-03-03 13:25:44 +0100

    jusqu'au conditionnelles

---
+++ src/Exo6.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exo6 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        boolean estPret;
+        int stockBalle;
+
+        System.out.println("Combien y a-t-il de balle dans le lanceur?");
+        stockBalle = sc.nextInt();
+
+        System.out.println("Etes-vous prêt?");
+        estPret = sc.nextBoolean();
+
+        if(estPret && stockBalle > 0){ // 1
+            System.out.println("Une balle est lancée");
+            stockBalle--;
+        }
+        else if(stockBalle <= 0){ // 2
+            System.out.println("Il n'y pas assez de balle");
+        }
+        else { // 3
+            System.out.println("non pret");
+        }
+
+        // estPret | stockBalle > 0 -> 1
+        // estPret | stockBalle <= 0 -> 2
+        // !estPret | stockBalle > 0 -> 3
+        // !estPret | stockBalle <= 0 -> 2
+    }
+}
---
+++ src/Exo7.java
@@ -0,0 +1,16 @@
+public class Exo7 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(2 +" * "+ i + " = "+ (2*i));
+        }
+
+        int cpt = 1;
+        while(cpt < 11){
+            System.out.println(2 +" * "+ cpt + " = "+ (2*cpt));
+            cpt++;
+        }
+
+    }
+}
