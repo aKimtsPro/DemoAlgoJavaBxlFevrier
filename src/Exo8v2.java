27249d871e360740ba96d8a602a07ad3b57dd09d
Admin <alexandre.kimtsaris@bstorm.be>
2021-03-03 14:46:23 +0100

    -> exo 14

---
+++ src/Exo8v2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exo8v2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int stock;
+        boolean pret;
+
+        do {
+            System.out.println("stock?");
+            stock = sc.nextInt();
+            System.out.println("Pret?");
+            pret = sc.nextBoolean();
+        } while( stock <= 0 || !pret );
+
+        while( stock > 0 ){
+            System.out.println("Lancer (reste : "+ --stock +")");
+        }
+
+        System.out.println("plus de balles");
+    }
+
+}
